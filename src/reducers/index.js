054086cf041348f3dcb53e673d9325abde5fb7ea
Andresdst <andres-network@hotmail.com>
2020-10-25 23:10:59 -0400

    bug arreglado en reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,10 @@ const reducer = (state, action) => {
 
   switch (action.type) {
     case 'SET_FAVORITE':
+      console.log(state)
       return {
         ...state, //el resto del estado
-        myList: [...state.mylist/*el resto de myList*/,action.payload/*nuevo objeto*/],
+        mylist: [...state.mylist/*el resto de myList*/,action.payload/*nuevo objeto*/],
       };
     default:
       return state;
